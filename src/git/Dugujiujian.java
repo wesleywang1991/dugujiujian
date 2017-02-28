25683a8bec23a5730825456f2ac78d9c8d4b0285
wesleywang <wesleywang1991@outlook.com>
2017-02-28 08:47:48 +0800

    update003

--- src/git/Dugujiujian.java
+++ src/git/Dugujiujian.java
@@ -4,6 +4,7 @@ package git;
 public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("dugujiujian11111111111");
+		System.out.println("dugujiujian22222222222");
 	}
 
 }
